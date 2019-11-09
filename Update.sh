4719caa926f0e5ccd7ddae4a841d37981dd485b1
Colby <cfrey@protonmail.com>
2019-11-08 16:06:18 -0800

    test

--- Update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# A simple script
-apt-get update
-apt-get upgrade
-apt-get autoremove
-apt-get autoclean
-
-ufw status
--- Windows Scripts/test.ps1
+++
@@ -1,2 +0,0 @@
-(gc regedit.reg) -replace '"EnableFirewall"=dword:00000001', '"EnableFirewall"=dword:00000000' -replace '"EnableFirewall"=dword:00000000', '"EnableFirewall"=dword:00000000' | Out-File regedit.reg
-pause
