dd8711f16b12bbf0baa05d581d1d216936d4e712
ColbyFr <53286217+ColbyFr@users.noreply.github.com>
2019-12-02 20:23:46 -0800

    Update Update.sh
    s

--- Update.sh
+++ Update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# A simple script
+# A simple script that updates and upgrades the system
+# The script also disables IPv6 and installs/enables UFW firewall
 apt-get update
 apt-get upgrade
 apt-get autoremove
 apt-get autoclean
 
-ufw status
+apt-install ufw
